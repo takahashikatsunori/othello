7707df5dfef3b5b9ccc82654116cfe41b5aea6df
takahashikatsunori <ktgalaxys@gmail.com>
2015-09-23 09:14:54 +0000

    add initial function

--- othello.cpp
+++ othello.cpp
@@ -3,8 +3,50 @@
 */
 #include "othello.h"
 
+void printb(BOARD* b)
+{
+    int x,y;
+    printf("=======================================\n");
+    if(b->turn==BLACK) printf(" TURN : BLACK\n"); else printf(" TURN : WHITE\n");
+    if(b->ispassed!=0) printf(" PREVIOUS TURN was Passed.\n"); else printf("\n");
+
+    for(y=0;y<8;y++)
+    {
+        printf("  +---+---+---+---+---+---+---+---+\n");
+        printf("  ");
+        for(x=0;x<8;x++)
+        {
+            switch(b->piece[y][x])
+            {
+                case WHITE:
+                    printf("| O ");break;
+                case BLACK:
+                    printf("| $ ");break;
+                default:
+                    printf("|   ");break;
+            };
+        };
+        printf("|\n");
+    }
+    printf("  +---+---+---+---+---+---+---+---+\n");
+}
+
+int resolve(char* path)
+{
+    return 0;
+}
+
 int main(int argc, char** argv)
 {
-    printf("hello,othello world.\n");
+    BOARD b;
+
+    if( argc < 2 ) {
+        memcpy(&b, &Initial_Board, sizeof(BOARD));
+    } else {
+        printf("currently, resume function is not implemented.\n");
+        return 0;
+    }
+
+    printb(&b);
 }
 
--- othello.h
+++ othello.h
@@ -1,8 +1,34 @@
 /*
     othello.h
 */
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <stdio.h>
+#include <string.h>
 
 #define WHITE	1
 #define BLACK	0
+
+#define RESOLV_WHITE_WIN 1
+#define RESOLV_BLACK_WIN 0
+#define RESOLV_DRAW (-1)
+
+typedef struct {
+    int piece[8][8];
+    int turn;
+    int ispassed;
+} BOARD;
+
+static BOARD Initial_Board = {
+    { -1, -1, -1, -1, -1, -1, -1, -1,
+      -1, -1, -1, -1, -1, -1, -1, -1,
+      -1, -1, -1, -1, -1, -1, -1, -1,
+      -1, -1, -1,  1,  0, -1, -1, -1,
+      -1, -1, -1,  0,  1, -1, -1, -1,
+      -1, -1, -1, -1, -1, -1, -1, -1,
+      -1, -1, -1, -1, -1, -1, -1, -1,
+      -1, -1, -1, -1, -1, -1, -1, -1},
+    0,
+    0
+};
 
